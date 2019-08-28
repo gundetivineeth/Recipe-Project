feac771e7b7f0a9ca18ca5f8291dd4f8c7da980f
vineethreddy <vineethg508@gmail.com>
2019-08-28 11:42:31 +0530

    Basic Project

---
+++ src/main/java/com/example/recipe/recipepreparation/RecipepreparationApplication.java
@@ -0,0 +1,14 @@
+package com.example.recipe.recipepreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipepreparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipepreparationApplication.class, args);
+        System.out.println("hello1");
+    }
+
+}
---
+++ src/test/java/com/example/recipe/recipepreparation/RecipepreparationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.recipe.recipepreparation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipepreparationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

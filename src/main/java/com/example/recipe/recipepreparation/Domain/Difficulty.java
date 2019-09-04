d11988c228f95eec5fa0ef27fae0ddb16504fd2e
gundetivineeth <vineethg508@gmail.com>
2019-09-04 20:09:57 +0530

    Adding many to many relationship

---
+++ src/main/java/com/example/recipe/recipepreparation/Domain/Difficulty.java
@@ -0,0 +1,8 @@
+package com.example.recipe.recipepreparation.Domain;
+
+import javax.persistence.Entity;
+
+
+public enum Difficulty {
+    EASY,MODERATE,KIND_OF_HARD,HARD
+}
--- src/main/java/com/example/recipe/recipepreparation/Domain/Recipe.java
+++ src/main/java/com/example/recipe/recipepreparation/Domain/Recipe.java
@@ -18,12 +18,19 @@ public class Recipe {
     private String directions;
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
     private Set<Ingredient> ingredientS;
 
+    @ManyToMany
+    @JoinTable(name="recipe_category",
+    joinColumns = @JoinColumn(name="recipe_id"),inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -111,5 +118,21 @@ public class Recipe {
 
     public void setIngredientS(Set<Ingredient> ingredientS) {
         this.ingredientS = ingredientS;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
